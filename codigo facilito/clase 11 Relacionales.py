d5169c0970f4d664755b804ef0624070b99b3558
Ivan Mamani <ivan.mamani.q@ucb.edu.bo>
2023-03-13 23:24:28 -0400

    13 de marso me gano codigo facilito

---
+++ codigo facilito/clase 11 Relacionales.py
@@ -0,0 +1,12 @@
+numero_uno= 10
+numero_dos= 20
+"""
+    >
+    <
+    >=
+    <=
+    ==
+    !=
+"""
+resulto = numero_uno < numero_dos
+print(resulto)
---
+++ pildoras_informaticas/video 06 Funciones II.py
@@ -0,0 +1,11 @@
+def suma(num1, num2):#resive parametros
+    #num1 = 5
+    #num2 = 7
+    #print(num1+num2)
+    res=num1+num2
+    return res
+print(suma(5,8))
+almacenamieto_resultado=suma(25,25)
+print(almacenamieto_resultado)
+print(suma(7,3))
+print(suma(35,558))
